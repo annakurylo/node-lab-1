dc30315bc97d25f59603387571f091f90fe464b6
annakurylo <anna.k.2003.g@gmail.com>
2022-10-18 15:21:16 +0300

    add integration of eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,13 @@
 module.exports = {
-  env: {
-    node: true,
-    es2021: true
-  },
-  extends: 'standard',
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  rules: {}
+    env: {
+        node: true,
+        es2021: true,
+    },
+    extends: ['standard', 'prettier'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {},
 }
