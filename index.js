90de55824d79bd48791bbc6d92a702b6730030e8
annakurylo <anna.k.2003.g@gmail.com>
2022-10-18 19:57:53 +0300

    add index.js handler aws

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export const handler = async (event) => {
     const max = 999
-    const val = _.random(max)
+    const val = 0
     const response = {
         statusCode: 200,
         body: `The random value (max ${max}) is: ${val}`,
