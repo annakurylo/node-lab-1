f1feb01731050c238ce823387b6b86fd023123b0
annakurylo <anna.k.2003.g@gmail.com>
2022-10-18 19:56:49 +0300

    add index.js handler

---
+++ index.js
@@ -0,0 +1,9 @@
+export const handler = async (event) => {
+    const max = 999
+    const val = _.random(max)
+    const response = {
+        statusCode: 200,
+        body: `The random value (max ${max}) is: ${val}`,
+    }
+    return response
+}
